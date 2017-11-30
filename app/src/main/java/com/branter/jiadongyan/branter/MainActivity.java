f15f4130448ed10aa4a62f54718c7ab3ce052236
xyjiang94 <xyjiang94@gmail.com>
2017-11-30 15:24:55 -0500

    ui

--- app/src/main/java/com/branter/jiadongyan/branter/MainActivity.java
+++ app/src/main/java/com/branter/jiadongyan/branter/MainActivity.java
@@ -316,7 +316,7 @@ class CSCTest extends AsyncTask<String, Void, Void> {
 //        csc.updateAccount("yo","true","11-12-2017");
 //        System.out.println(csc.signIn("xxx@xxx","pass"));
 //        csc.createPost("1","hello world!!!!");
-        csc.createEvent(new String[] {"title"},new String[] {"mysterious event"});
+//        csc.createEvent(new String[] {"title"},new String[] {"mysterious event"});
 
 //        csc.createEvent(new String[] {"title"},new String[] {"mysterious event1"});
 
