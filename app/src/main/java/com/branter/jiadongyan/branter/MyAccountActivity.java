7be43c26cb60a8e3ac2f8816581daec472f9fc39
Spencerzy <spencerzycqq@gmail.com>
2017-11-30 09:15:38 -0500

    finish profile update

--- app/src/main/java/com/branter/jiadongyan/branter/MyAccountActivity.java
+++ app/src/main/java/com/branter/jiadongyan/branter/MyAccountActivity.java
@@ -1,11 +1,13 @@
 package com.branter.jiadongyan.branter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MyAccountActivity extends AppCompatActivity {
 
@@ -15,15 +17,24 @@ public class MyAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_account);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button but1 = (Button)findViewById(R.id.profile_update_button);
+        Button but2 = (Button)findViewById(R.id.profile_back_button);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        but1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v){
+                startActivity(new Intent("com.branter.jiadongyan.branter.ProfileUpdateActivity"));
             }
         });
+
+        but2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+
+
     }
 
 }
