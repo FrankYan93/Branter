a0246a99b487f9a35d4dd4a4643f86f425258e56
icadi <qiaofeng628@gmail.com>
2017-11-30 17:43:31 -0500

    while headphoto

--- app/src/main/java/com/branter/jiadongyan/branter/MainActivity.java
+++ app/src/main/java/com/branter/jiadongyan/branter/MainActivity.java
@@ -260,7 +260,7 @@ public class MainActivity extends AppCompatActivity
             gridTest = new GridTest();
             switch (i){
                 case 0:gridTest.setEventTitle("Football Game");
-                    gridTest.setHeadphoto("http://www.gx8899.com/uploads/allimg/160804/3-160P4111639.jpg");
+                    gridTest.setHeadphoto("https://pbs.twimg.com/media/DP6iGTBWkAErwgJ.jpg");
                     gridTest.setContent("This is an exciting football game event");
                     gridTest.setTime("From 2017-11-28 to 2017-12-02");
                     gridTest.setImage("https://i.pinimg.com/originals/b3/fc/31/b3fc31345130e0d09b54985bedb2d95c.jpg");
@@ -268,7 +268,7 @@ public class MainActivity extends AppCompatActivity
                     break;
                 case 1:
                     gridTest.setEventTitle("Celtics");
-                    gridTest.setHeadphoto("http://img3.imgtn.bdimg.com/it/u=3367770910,1075442079&fm=21&gp=0.jpg");
+                    gridTest.setHeadphoto("https://pbs.twimg.com/media/DP6iGTBWkAErwgJ.jpg");
                     gridTest.setContent("This is for you who are interested in celtics");
                     gridTest.setTime("From 2017-11-20 to 2017-11-28");
                     gridTest.setImage("http://static2.businessinsider.com/image/56c3485d6e97c623048b7658-1190-625/the-boston-celtics-are-the-nbas-biggest-x-factor-2-days-before-the-trade-deadline.jpg");
@@ -276,7 +276,7 @@ public class MainActivity extends AppCompatActivity
                     break;
                 case 2:
                     gridTest.setEventTitle("Concert");
-                    gridTest.setHeadphoto("http://img5q.duitang.com/uploads/item/201404/03/20140403135406_XFS3M.jpeg");
+                    gridTest.setHeadphoto("https://pbs.twimg.com/media/DP6iGTBWkAErwgJ.jpg");
                     gridTest.setContent("This is a good concert");
                     gridTest.setTime("From 2017-11-12 to 2017-11-20");
                     gridTest.setImage("https://static.pexels.com/photos/196652/pexels-photo-196652.jpeg");
@@ -284,7 +284,7 @@ public class MainActivity extends AppCompatActivity
                     break;
                 case 3:
                     gridTest.setEventTitle("Gourmet Festival");
-                    gridTest.setHeadphoto("http://img3.imgtn.bdimg.com/it/u=3367770910,1075442079&fm=21&gp=0.jpg");
+                    gridTest.setHeadphoto("https://pbs.twimg.com/media/DP6iGTBWkAErwgJ.jpg");
                     gridTest.setContent("Have a dinner at gourmet festival");
                     gridTest.setTime("From 2017-11-10 to 2017-11-18");
                     gridTest.setImage("https://www.ggcatering.com/system/uploads/fae/image/asset/2954/Global_Gourmet_hp_banner_3.jpg");
