19f1ac5c84d77cbda01d5bed600f846f1eb69200
icadi <qiaofeng628@gmail.com>
2017-11-29 22:41:15 -0500

    delete Chinese

--- app/src/main/java/com/branter/jiadongyan/branter/GridViewAdapter.java
+++ app/src/main/java/com/branter/jiadongyan/branter/GridViewAdapter.java
@@ -27,8 +27,8 @@ public class GridViewAdapter extends BaseAdapter {
         this.context = context;
         this.list = list;
         this.wh=(SysUtils.getScreenWidth(context)-SysUtils.Dp2Px(context, 99))/3;
-        this.finaImageLoader = FinalBitmap.create(context);/*获取一个FinalBitmap对象*/
-        this.finaImageLoader.configLoadfailImage(R.drawable.ic_menu_gallery);/*图片加载完成前显示的图片*/
+        this.finaImageLoader = FinalBitmap.create(context);
+        this.finaImageLoader.configLoadfailImage(R.drawable.ic_menu_gallery);
     }
 
     @Override
@@ -51,16 +51,13 @@ public class GridViewAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
-        Holder holder;//java内部类
+        Holder holder;
         if(view==null){
             view = LayoutInflater.from(context).inflate(R.layout.item_gridview,null);
             holder = new Holder();
             holder.imageView = (ImageView)view.findViewById(R.id.imagevView);
             view.setTag(holder);
-            /*首先我们要知道setTag方法是干什么的，他是给View对象的一个标签，标签可以是任何内容，
-            我们这里把他设置成了一个对象，因为我们是把item_gridview.xml的元素抽象出来成为一个类ViewHolder，
-            用了setTag，这个标签就是ViewHolder实例化后对象的一个属性。我们之后对于ViewHolder实例化的对象holder的操作，
-            都会因为Java的引用机制而一直存活并改变convertView的内容，而不是每次都是去new一个。我们就这样达到的重用*/
+
         }
         else{
             holder = (Holder)view.getTag();
@@ -68,8 +65,7 @@ public class GridViewAdapter extends BaseAdapter {
 
         finaImageLoader.display(holder.imageView, list.get(position));
         AbsListView.LayoutParams param = new AbsListView.LayoutParams(wh,wh);
-       /* 创建一个布局（LayoutParams）的实例 param。
-        AbsListView.LayoutParams(wh,wh) 指定了该布局的宽和高；*/
+
         view.setLayoutParams(param);
         return view;
 
--- app/src/main/java/com/branter/jiadongyan/branter/ListViewAdapter.java
+++ app/src/main/java/com/branter/jiadongyan/branter/ListViewAdapter.java
@@ -81,12 +81,12 @@ public class ListViewAdapter  extends BaseAdapter implements Filterable {
         if(convertView==null){
             convertView = mInflater.inflate(R.layout.item_listview,null);
             holder = new ViewHolder();
-            holder.headphoto = (ImageView) convertView.findViewById(R.id.info_iv_head);//头像
-            holder.disName = (TextView) convertView.findViewById(R.id.info_tv_name);//昵称
-            holder.time = (TextView) convertView.findViewById(R.id.info_tv_time);//时间
-            holder.content = (TextView) convertView.findViewById(R.id.info_tv_content);//发布内容
-            holder.rl4=(RelativeLayout) convertView.findViewById(R.id.rl4);//图片布局
-            holder.gv_images = (MyGridView) convertView.findViewById(R.id.gv_images);//图片
+            holder.headphoto = (ImageView) convertView.findViewById(R.id.info_iv_head);
+            holder.disName = (TextView) convertView.findViewById(R.id.info_tv_name);
+            holder.time = (TextView) convertView.findViewById(R.id.info_tv_time);
+            holder.content = (TextView) convertView.findViewById(R.id.info_tv_content);
+            holder.rl4=(RelativeLayout) convertView.findViewById(R.id.rl4);
+            holder.gv_images = (MyGridView) convertView.findViewById(R.id.gv_images);
             convertView.setTag(holder);
         }else {
             holder = (ViewHolder)convertView.getTag();
@@ -100,27 +100,27 @@ public class ListViewAdapter  extends BaseAdapter implements Filterable {
             headpath = gridTest.getHeadphoto();
             contentimage =gridTest.getImage();
         }
-        //昵称
+
         if (name!=null&&!name.equals("")) {
             holder.disName.setText(name);
         }
-        //是否含有图片
+
         if (contentimage!=null&&!contentimage.equals("")) {
             holder.rl4.setVisibility(View.VISIBLE);
             initInfoImages(holder.gv_images,contentimage);
         } else {
             holder.rl4.setVisibility(View.GONE);
         }
-        //发布时间
+
         if (time!=null&&!time.equals("")) {
             holder.time.setText(time);
         }
-        //内容
+
         if (content!=null&&!content.equals("")) {
             holder.content.setText(content);
             Linkify.addLinks(holder.content, Linkify.WEB_URLS);
         }
-        //头像
+
         if (headpath!=null&&!headpath.equals("")) {
             finalBitmap.display(holder.headphoto,headpath);
         } else {
@@ -170,12 +170,7 @@ public class ListViewAdapter  extends BaseAdapter implements Filterable {
             }
             RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(w, RelativeLayout.LayoutParams.WRAP_CONTENT);
             gv_images.setLayoutParams(lp);
-            /*第一个参数为宽的设置，第二个参数为高的设置。
-            如果将一个View添加到一个Layout中，最好告诉Layout用户期望的布局方式，也就是将一个认可的layoutParams传递进去
-            但LayoutParams类也只是简单的描述了宽高，宽和高都可以设置成三种值：
-            1，一个确定的值；
-            2，FILL_PARENT，即填满（和父容器一样大小）；
-            3，WRAP_CONTENT，即包裹住组件就好。。*/
+
             gridViewAdapter=new GridViewAdapter(context, list);
             gv_images.setAdapter(gridViewAdapter);
             gv_images.setOnItemClickListener(new AdapterView.OnItemClickListener() {
